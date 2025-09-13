26f17f6bf9879ef3366baa249606971b017e0182
yuridazani <117534278+yuridazani@users.noreply.github.com>
2025-09-13 14:27:03 +0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { AuthProvider } from './context/AuthContext'; // Import
+import { AuthProvider } from './context/AuthContext';
+import { ModalProvider } from './context/ModalContext'; // Import
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider> {/* Bungkus App dengan AuthProvider */}
-      <App />
+    <AuthProvider>
+      <ModalProvider> {/* Bungkus di sini */}
+        <App />
+      </ModalProvider>
     </AuthProvider>
   </React.StrictMode>,
 );
